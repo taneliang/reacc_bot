1a3f7053e9fe83d5596b28755cb69c6c0494677a
E-Liang Tan <eliang@eliangtan.com>
2019-02-14 12:41:14 +0800

    Move reactions to a txt file

--- bot.rb
+++ bot.rb
@@ -3,80 +3,6 @@ require 'telegram/bot'
 
 token = ENV['BOT_TOKEN']
 
-replies = [
-  'LOL',
-  'lol',
-  'Lol',
-  'oh lol',
-  'lmao',
-  'Lmao',
-  'Rip',
-  'rip',
-  'Wew',
-  'wew',
-  'Kek',
-  'kek',
-  'Hahaha',
-  'HAHA',
-  'Hahahaha',
-  'wtf',
-  'Wtf',
-  'Nice',
-  'Sick',
-  'issit',
-  'Chey',
-  'wat',
-  'gg',
-  'H A C K E R M A N',
-  'You should out elitist them!',
-  'xD',
-  'sgtm',
-  'lgtm',
-  'How about no',
-  'erm',
-  '^',
-  'cri',
-  'RT',
-  'ded',
-  'damn',
-  '可以吃的吗？',
-  '?',
-  'ah',
-  '/goodbot',
-  '/badbot',
-  '/bigthonk',
-  '/wasted',
-  '...',
-  'Lol wtf',
-  'Ikr',
-  ':<',
-  'famous last words',
-  'STOP',
-  'plsno',
-  'ohno',
-  'Uh oh',
-  'No way',
-  'EXCUSE YOU',
-  'I see',
-  '🙃',
-  '😂',
-  'why?',
-  'XD',
-  'Same lel',
-  'seriously?',
-  'Really meh hahaha',
-  'For now',
-  'YAY',
-  'Errrok',
-  'Wow rekt',
-  'OH MY GOD',
-  'you_dont_cb.png',
-  'meh',
-  'Fak u',
-  'Diu',
-  '😂ok'
-]
-
 def should_send
   rand(90).zero?
 end
@@ -86,9 +12,10 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     p message
     if message.text == '/help' || message.text == '/start' || should_send
+      reply = File.readlines('reaccs.txt').sample
       bot.api.send_message(chat_id: message.chat.id,
                            reply_to_message_id: message.message_id,
-                           text: replies.sample)
+                           text: reply)
     end
   end
 end
