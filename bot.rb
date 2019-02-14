f3a39e835461f8c52e769fbaf7abe6a8af69e37b
E-Liang Tan <eliang@eliangtan.com>
2019-02-14 16:00:46 +0800

    Implement feedback and reaction storage

--- bot.rb
+++ bot.rb
@@ -1,5 +1,6 @@
 require 'dotenv/load'
 require 'telegram/bot'
+require './db'
 
 token = ENV['BOT_TOKEN']
 
@@ -14,8 +15,14 @@ end
 
 def msg_to_logstr(message)
   return "" unless message
-  "MSGTXT \"#{message.text}\" FROM #{user_to_logstr(message.from)} DATE #{message.date} TYPE #{message.chat&.type} TITLE \"#{message.chat&.title}\" CHATID #{message.chat&.id}"
+  "MSGTXT \"#{message.text}\" FROM #{user_to_logstr(message.from)} MSGID #{message.message_id} DATE #{message.date} TYPE #{message.chat&.type} TITLE \"#{message.chat&.title}\" CHATID #{message.chat&.id}"
 end
+
+reply_keyboard = [[
+  Telegram::Bot::Types::InlineKeyboardButton.new(text: '👍', callback_data: 'goodreacc'),
+  Telegram::Bot::Types::InlineKeyboardButton.new(text: '👎', callback_data: 'badreacc'),
+]]
+markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: reply_keyboard)
 
 def log_incoming(message)
   puts "MSG: #{msg_to_logstr(message)}"
@@ -29,40 +36,64 @@ def log_reacc(trigger_msg, reacc_msg)
 
   puts "REACC: \"#{trigger_msg.text}\" -> \"#{reacc_msg["result"]["text"]}\" REACCMSGID #{reacc_msg["result"]["message_id"]} ||| TRIGGER #{msg_to_logstr(trigger_msg)}"
 
-  # TODO: Store reaction
+  # Store reaction
+  insert_reaction(trigger_msg, reacc_msg)
 end
 
-def handle_feedback(feedback_msg)
-  puts "FEEDBACK: #{feedback_msg.data} FEEDBID #{feedback_msg.id} ||| REACC #{msg_to_logstr(feedback_msg.message)} ||| TRIGGER #{msg_to_logstr(feedback_msg.message&.reply_to_message)}"
-  # TODO: Store feedback
+def handle_feedback(feedback_msg, bot, markup)
+  puts "FEEDBACK: #{feedback_msg.data} FEEDBID #{feedback_msg.id} FROM #{user_to_logstr(feedback_msg.from)} ||| REACC #{msg_to_logstr(feedback_msg.message)} ||| TRIGGER #{msg_to_logstr(feedback_msg.message&.reply_to_message)}"
+
+  # Store feedback
+  reacc_message_id = feedback_msg.message&.message_id
+  return if reacc_message_id == nil
+
+  case feedback_msg.data
+  when "goodreacc"
+    increment_upvotes(reacc_message_id)
+  when "badreacc"
+    increment_downvotes(reacc_message_id)
+  end
+
   # TODO: Edit reacc with feedback score
+  # bot.api.edit_message_text(chat_id)
+  get_votes(reacc_message_id) do |row|
+    reaction, upvotes, downvotes = row
+    new_text = "#{reaction}\n_Verdict: #{upvotes-downvotes < 0 ? "👎" : "👍"} #{upvotes-downvotes}_"
+    bot.api.edit_message_text(chat_id: feedback_msg.message&.chat&.id,
+                              message_id: reacc_message_id,
+                              text: new_text,
+                              parse_mode: "Markdown",
+                              reply_markup: markup)
+  end
 end
 
-reply_keyboard = [[
-  Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Good reacc', callback_data: 'goodreacc'),
-  Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Bad reacc', callback_data: 'badreacc'),
-]]
-markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: reply_keyboard)
+init_db
 
 Telegram::Bot::Client.run(token) do |bot|
   puts 'Started'
   bot.listen do |message|
-    case message
-    when Telegram::Bot::Types::CallbackQuery
-      handle_feedback(message)
-      bot.api.answerCallbackQuery(callback_query_id: message.id, 'text': "Thanks for the reacc reacc!")
+    begin
+      case message
+      when Telegram::Bot::Types::CallbackQuery
+        handle_feedback(message, bot, markup)
+        bot.api.answerCallbackQuery(callback_query_id: message.id, 'text': "Thanks for the reacc reacc!")
 
-    when Telegram::Bot::Types::Message
-      log_incoming(message)
-      if message.text == '/help' || message.text == '/start' || should_send
-        reacction = File.readlines('reaccs.txt').sample
-        reacction.strip!
-        reacc_msg = bot.api.send_message(chat_id: message.chat.id,
-                                         reply_to_message_id: message.message_id,
-                                         text: reacction,
-                                         reply_markup: markup)
-        log_reacc(message, reacc_msg)
+      when Telegram::Bot::Types::Message
+        log_incoming(message)
+        if message.text == '/help' || message.text == '/start' || should_send
+          reacction = File.readlines('reaccs.txt').sample
+          reacction.strip!
+          reacc_msg = bot.api.send_message(chat_id: message.chat.id,
+                                           reply_to_message_id: message.message_id,
+                                           text: reacction,
+                                           parse_mode: "Markdown",
+                                           reply_markup: markup)
+          log_reacc(message, reacc_msg)
+        end
       end
+    rescue => e
+      puts "Exception occurred"
+      p e
     end
   end
 end
